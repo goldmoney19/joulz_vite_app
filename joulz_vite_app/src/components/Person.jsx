a9e42b79bedbe4ccb58d1663e7ed545f45cc3978
goldmoney19 <57579232+goldmoney19@users.noreply.github.com>
2025-06-30 09:57:30 -0700

    Update Person.jsx

--- joulz_vite_app/src/components/Person.jsx
+++ joulz_vite_app/src/components/Person.jsx
@@ -30,7 +30,7 @@ const navigate = useNavigate();
                 const fetchData = async(req, res) => {
 
                 try{
-         const response = await axios.get("http://localhost:8000/api/portfolios" , {
+         const response = await axios.get("https://joulz-backend.onrender.com/api/portfolios" , {
                 headers:{
                           Authorization:`Bearer ${token}`
 
@@ -50,7 +50,7 @@ fetchData();
 
 
    const deletePortfolio = async (userId) => {
-         await axios.delete(`http://localhost:8000/api/delete/portfolio/${userId}`)
+         await axios.delete(`https://joulz-backend.onrender.com/api/delete/portfolio/${userId}`)
         .then((response) => {
         setPortfolio((prevUser)=>prevUser.filter((user)=>user._id !==userId))
 
