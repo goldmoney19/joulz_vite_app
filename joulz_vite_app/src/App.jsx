6ad2aa6d66026c79212500b1489a02327d6821aa
goldmoney19 <57579232+goldmoney19@users.noreply.github.com>
2025-06-30 14:04:13 -0700

    Update App.jsx

--- joulz_vite_app/src/App.jsx
+++ joulz_vite_app/src/App.jsx
@@ -53,7 +53,7 @@ function App() {
    <Route path = "/about" element = {<About />} />
    
 
-   <Route path = "/portfolio" element = {<Portfolio />} />
+
    
 
    <Route path = "/insertPortfolio" element = {
