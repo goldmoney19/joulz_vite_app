0a9bbc4b95e4461202caf8ed5e8ab7571ef293f8
goldmoney19 <57579232+goldmoney19@users.noreply.github.com>
2025-07-02 18:17:43 -0700

    Update jwtConfig.js

--- server/configuration/jwtConfig.js
+++ server/configuration/jwtConfig.js
@@ -1,5 +1,11 @@
-import crypto from "crypto"
+// import crypto from "crypto"
 
-const secretKey = crypto.randomBytes(32).toString('hex');
+// const secretKey = crypto.randomBytes(32).toString('hex');
 
-export default secretKey
+// export default secretKey
+
+
+
+
+const secretKey = process.env.JWT_SECRET; // This line gets the secret from the environment
+export default secretKey;
