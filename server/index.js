bdff3d41e6598ad39dc2a414de2ab1ba04d90a1c
goldmoney19 <dagold80s@gmail.com>
2025-07-02 23:46:48 -0700

    app cors

--- server/index.js
+++ server/index.js
@@ -17,18 +17,20 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+dotenv.config();
+const app = express();
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 const corsOptions = {
   origin: 'https://vercel.com/g-coder/joulz-vite-app', // <-- Replace with your actual Vercel frontend URL
   optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 app.use(cors(corsOptions));
 
-const app = express();
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(cors());
-dotenv.config();
+
+
+
 
 app.use('/uploads', express.static(path.join(__dirname, 'Uploads')));
 
