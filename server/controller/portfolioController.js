e7590d2b736246ff3c47ad96e3e03aa5d679d954
goldmoney19 <57579232+goldmoney19@users.noreply.github.com>
2025-07-06 12:33:24 -0700

    Update portfolioController.js

--- server/controller/portfolioController.js
+++ server/controller/portfolioController.js
@@ -51,7 +51,7 @@ console.log(req.files, 16);
 
  //const imagePath = image.path.replace('\\g', '');
          const newportf =  new Portfolio({title:title, description:description,
-          design:design, image:imagePath,multipleImages:multipleImages})
+          design:design, image:singleImageResult.secure_url, multipleImages:multipleImagesResults})
           const success = newportf.save();
  res.status(200).json(success)
      }
