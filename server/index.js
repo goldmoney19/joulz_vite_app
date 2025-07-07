4c5a861b27b4cb81bfc93c07d891b0b59c15577a
goldmoney19 <57579232+goldmoney19@users.noreply.github.com>
2025-07-06 23:00:09 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,10 +14,10 @@ import path from "path";
 import upload from "./services/upload.js"
 // import upload from "./configuration/multerConfig.js"
 
-import { fileURLToPath } from 'url';
+// import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(bodyParser.json());
