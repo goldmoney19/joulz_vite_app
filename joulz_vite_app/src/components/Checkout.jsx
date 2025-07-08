300b79436031004d8bd3426842ecd4ef61f97534
goldmoney19 <57579232+goldmoney19@users.noreply.github.com>
2025-07-08 11:53:12 -0700

    Update Checkout.jsx

--- joulz_vite_app/src/components/Checkout.jsx
+++ joulz_vite_app/src/components/Checkout.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
 import Footer from './Footer';
-import Navvbar from './Navvbar';
+import Navvbar from './components/Navvbar.jsx'
 
 
 
