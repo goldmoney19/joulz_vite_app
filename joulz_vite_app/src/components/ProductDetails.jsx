ecb43e10fe927d3847b9ff9a896bc6d9edf14d9d
goldmoney19 <57579232+goldmoney19@users.noreply.github.com>
2025-07-08 13:32:11 -0700

    Update ProductDetails.jsx

--- joulz_vite_app/src/components/ProductDetails.jsx
+++ joulz_vite_app/src/components/ProductDetails.jsx
@@ -6,7 +6,7 @@ import {useParams, useNavigate} from 'react-router-dom';
 import toast from 'react-hot-toast';
 import '../App.css'
 import Shop from './Shop';
-import Navvbar from './components/Navvbar.jsx';
+
 
 
 
@@ -76,7 +76,7 @@ const ProductDetails = () => {
 
      return  <div > 
              <Container fluid  >
-              <Navvbar />
+     
 <h4 className='details_text1'> Product Details</h4>
                 <Row >
 
