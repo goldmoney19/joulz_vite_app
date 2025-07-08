72f9ae6d8124e151d793b2d85e4f491e62d2eb84
goldmoney19 <57579232+goldmoney19@users.noreply.github.com>
2025-07-08 11:52:46 -0700

    Update Karuzel.jsx

--- joulz_vite_app/src/components/Karuzel.jsx
+++ joulz_vite_app/src/components/Karuzel.jsx
@@ -8,7 +8,7 @@ import telegram from '../assets/telelogo.png'
 import facebook from '../assets/facelogo.png'
 import whatsapp from '../assets/whatsapp-256.png'
 import instagram from '../assets/insta.png'
-import Navvbar from '.components/Navvbar.jsx'
+import Navvbar from './components/Navvbar.jsx'
 
 
 
