aee3673b4937bac10409b09f7ed6b833f9d6594f
goldmoney19 <57579232+goldmoney19@users.noreply.github.com>
2025-07-09 01:35:25 -0700

    Update Portfolio_details.jsx

--- joulz_vite_app/src/components/Portfolio_details.jsx
+++ joulz_vite_app/src/components/Portfolio_details.jsx
@@ -1,1 +1,118 @@
+import React from 'react'
+import {useState , useEffect} from 'react'
+import {Container, Row, Col,Nav,Button} from 'react-bootstrap'
+import axios from 'axios';
+import {useParams, useNavigate} from 'react-router-dom';
+import toast from 'react-hot-toast';
+import '../App.css'
+import Footer from './Footer';
+
+
+
+
+
+
+import Cookies from "cookie-universal"
+import PortfolioDisplay from './PortfolioDisplay';
+
+const cookies = new Cookies();
+   
+
+
+const Portfolio_details = () => {
+
+   const token = cookies.get("access-token");
+
+   const [penthause, setPenthause] = useState([]);
+   // const [productId, setProductId] = useState("");
+    //  const [quantity , setQuantity] =  useState("1");
+      
+      const {id} = useParams();
+    
+   
+  
+
+   useEffect(()=>{
+     const fetchUsers = async() => {
+                    try{
+            const response = await axios.get(`http://localhost:8000/api/portfolio/${id} `);
+
+               const result = await response.data;
+                             console.log(result); 
+                            setPenthause(result)
+    }
+                     catch (error) {
+     console.log("error while fetching", error) 
+    }
+     }
+        
+                    fetchUsers();
+               }, [id]);
+ const navigate = useNavigate();
+
+
+
+     return  <div  style={{paddingTop:"70px"}}>
+
+             <Container fluid >
+   
+                <h4 className='details_text1'> Portfolio Details</h4>
+               <Row >
+                    <>
+
+                     {penthause?.multipleImages?.map((img, index) => (
+                 <Col sm = {12} key ={index} >
+     <span><img  style={{height: '400px', width: '50%',marginTop:'40px',marginBottom:'40px'}}  src = {`http://localhost:8000/${img}`}></img></span>
+                 </Col>
+            ))}
+   
+                   
+                  </> 
+                   
+
+                </Row>
+                <br></br>
+                  <br></br>
+                   <Row>
+
+                  <Col sm={12}>
+                   <p className='' style={{fontFamily: "EB Garamond,serif"}}>{penthause.description}</p>
+                       <p className='' style={{fontWeight:"bold", fontFamily: "EB Garamond,serif"}} >{penthause.design}</p>
+                       
+                      
+                
+                  </Col>
+                </Row>
+
+                     <br></br>
+                       <br></br>
+                  <br></br>
+                  <Row>
+
+                  <Col sm={12}>
+                   <PortfolioDisplay />
+                       
+                      
+                
+                  </Col>
+                </Row>
+
+                     <br></br>
+                       <br></br>
+                  <br></br>
+                     <br></br>
+                <Row>
+
+                  <Col>
+                 
+                  <Footer />
+                  </Col>
+                </Row>
+
+             </Container>
+    
+          </div> 
+     }  
+
+     export default Portfolio_details
 
