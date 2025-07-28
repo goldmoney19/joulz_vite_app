14b992671d2138d9c46d961c070a4054c166eafc
goldmoney19 <57579232+goldmoney19@users.noreply.github.com>
2025-07-28 03:12:45 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ app.use(cookieParser());
 // ];
 
 app.use(cors({
-origin:'http://localhost:8082',
+origin:'http://localhost:8081',
    credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
