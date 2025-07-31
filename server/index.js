0bcd3916825b321c238e6a570b942c992f52fb4d
goldmoney19 <57579232+goldmoney19@users.noreply.github.com>
2025-07-31 09:23:59 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ app.use(cookieParser());
 // ];
 
 app.use(cors({
-origin:'https://joulz-vite-app.vercel.app',
+origin:'http://localhost:8081',
    credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
